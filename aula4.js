ea1afb90982423455992f4c07d5b949ea5abdaae
thiagonut <125615751+thiagonut@users.noreply.github.com>
2023-09-01 22:22:36 -0300

    Add files via upload

---
+++ aula4.js
@@ -0,0 +1,73 @@
+
+/* exercicio 1 */
+console.log(26);
+
+/*exercicio 2 */
+console.log("dias programados: "+ 1);
+
+/*exercicio 3*/
+//vai corinthians!
+
+
+/*exercicio 4*/
+const primeiraVar = "oi";
+let segundaVar = 777;
+var terceiraVar = true;
+var time = ["Corinthians", "Palmeiras"];
+ var caro = {
+    nome:'LEO'
+ }
+ /* exercicio 5*/
+ let idade = 20;
+ idade = idade + 3.5;
+
+ console.log(idade);
+
+ /*exercicio 6*/
+
+var dataInicio = new Date();
+var anoInicio  = dataInicio.getFullYear();
+var result = anoInicio - 1969;
+console.log(result)
+
+/*exercicio 7*/
+var conta = 65/240;
+console.log(conta);
+
+/*exercicio 8*/
+var multiplica = 0.2708*100
+console.log(multiplica)
+
+/*exercicio 9*/
+var concatena = ("Hello" + "world");
+console.log(concatena);
+
+/*exercicio 10*/
+var concatena = ("Hello " + "world");
+console.log(concatena);
+
+/*exercicio 11*/
+var string = "Teaching the world how to code";
+var empty = "";
+
+console.log("Teaching the world how to code possui: " +  string.length + "caracteres");
+
+/*exercicio 12*/
+console.log("oi".toUpperCase());
+
+/*exercicio 13*/
+var str = " Professor Kaian Novais ";
+console.log(str.trim());
+
+/*exercicio 14*/
+let numAleatorio = Math.random;
+let resul = numAleatorio*100;
+console.log(result);
+
+/*exercicio 15*/
+let ceil = 43.8
+console.log(Math.ceil(ceil));
+
+/*exercicio 16*/
+let num = 2017;
+console.log(Number.isInteger(num));
